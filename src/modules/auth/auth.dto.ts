e14d1e8b8810654b294a3ca758cd779bd308e851
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-09-07 09:07:56 +0600

    updating zod validation

--- src/modules/auth/auth.dto.ts
+++ src/modules/auth/auth.dto.ts
@@ -1,19 +1,11 @@
 import { z } from "zod";
-import { Gender, Sex, UserRole } from "../user/user.constant";
+import { UserRole } from "../user/user.constant";
 
 export const registerSchema = z.object({
   email: z.email("Invalid email address"),
   password: z.string().min(6, "Password must be at least 6 characters"),
-  role: z.enum(UserRole).default(UserRole.User),
-  firstName: z.string().min(1, "First name is required"),
-  surname: z.string().min(1, "Surname is required"),
-  dateOfBirth: z
-    .string()
-    .regex(/^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/\d{4}$/, {
-      message: "Date of Birth must be in format dd/mm/yyyy",
-    }),
-  gender: z.enum(Gender),
-  sex: z.enum(Sex),
+  role: z.enum(UserRole).default(UserRole.Employee),
+  name: z.string().min(1, "Name is required"),
   deviceTokens: z.string().optional(),
 });
 

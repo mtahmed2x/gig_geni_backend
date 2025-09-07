4bdab58ab4131e43a8d42b92418a37f567fc0122
Mir Tanim Ahmed <tanim@rentyard.com>
2025-09-07 16:20:24 +0600

    added quiz question generated by AI

--- src/modules/quizQuestion/quizQuestion.route.ts
+++ src/modules/quizQuestion/quizQuestion.route.ts
@@ -1,12 +1,30 @@
 import { Router } from "express";
 import { quizQuestionController } from "./quizQuestion.controller";
+import { auth } from "../../middlewares/auth";
+import { UserRole } from "../user/user.constant";
 
 const router: Router = Router();
 
-router.post("/", quizQuestionController.createQuizQuestion);
+router.post(
+  "/create",
+  auth(UserRole.Employer),
+  quizQuestionController.createQuizQuestion
+);
 router.get("/", quizQuestionController.getAllQuizQuestion);
-router.get("/:id", quizQuestionController.getQuizQuestionById);
-router.patch("/:id", quizQuestionController.updateQuizQuestion);
-router.delete("/:id", quizQuestionController.deleteQuizQuestion);
+router.get(
+  "/:id",
+  auth(UserRole.Employer),
+  quizQuestionController.getQuizQuestionById
+);
+router.patch(
+  "/update/:id",
+  auth(UserRole.Employer),
+  quizQuestionController.updateQuizQuestion
+);
+router.delete(
+  "/delete/:id",
+  auth(UserRole.Employer),
+  quizQuestionController.deleteQuizQuestion
+);
 
 export const quizQuestionRoutes = router;

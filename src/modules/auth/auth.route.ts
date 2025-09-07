12e4393630f39e2eee4573cd1db7ddd33a998e71
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-09-07 16:17:51 +0600

    fixing routes in auth

--- src/modules/auth/auth.route.ts
+++ src/modules/auth/auth.route.ts
@@ -6,10 +6,22 @@ import { UserRole } from "../user/user.constant";
 const router: Router = Router();
 
 router.post("/register", authController.register);
-router.post("/verifyOTP", auth(UserRole.User), authController.verifyOTP);
-router.post("/resendOTP", auth(UserRole.User), authController.resendOTP);
+router.post(
+  "/verifyOTP",
+  auth(UserRole.Employee, UserRole.Employer),
+  authController.verifyOTP
+);
+router.post(
+  "/resendOTP",
+  auth(UserRole.Employee, UserRole.Employer),
+  authController.resendOTP
+);
 router.post("/login", authController.login);
-router.post("/logout", auth(UserRole.User), authController.logout);
+router.post(
+  "/logout",
+  auth(UserRole.Employee, UserRole.Employer),
+  authController.logout
+);
 // router.get("/:id", authController.getAuthById);
 // router.patch("/:id", authController.updateAuth);
 // router.delete("/:id", authController.deleteAuth);

177a4a2d2057f165b1029e0d6ccc29f311506bf6
Mir Tanim Ahmed <tanim@rentyard.com>
2025-09-07 07:51:05 +0600

    adding file upload support to cloudinary

---
+++ src/middlewares/fileHandler.ts
@@ -0,0 +1,63 @@
+import Cloudinary from "../utils/cloudinary";
+import { NextFunction, Request, Response } from "express";
+import { FileArray, UploadedFile } from "express-fileupload";
+import createError from "http-errors";
+import { StatusCodes } from "http-status-codes";
+
+const uploadFileToCloudinary = async (
+  file: UploadedFile,
+  folder: string
+): Promise<string> => {
+  try {
+    return await Cloudinary.upload(file, folder);
+  } catch (error: any) {
+    throw new Error(`Failed to upload ${folder} file: ${error.message}`);
+  }
+};
+
+export const fileHandler = async (
+  req: Request,
+  _res: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const fileFields = [
+      { fieldName: "bannerImage", folder: "gig-geni/competition/bannerImage" },
+    ];
+
+    const files = req.files as FileArray | undefined;
+    console.log(files);
+
+    if (files) {
+      await Promise.all(
+        fileFields.map(async ({ fieldName, folder }) => {
+          const file = files[fieldName];
+          console.log(file);
+
+          if (file) {
+            if (Array.isArray(file)) {
+              const fileUrls = await Promise.all(
+                file.map((fileItem: UploadedFile) =>
+                  uploadFileToCloudinary(fileItem, folder)
+                )
+              );
+              req.body[fieldName] = fileUrls;
+            } else {
+              const fileUrl = await uploadFileToCloudinary(
+                file as UploadedFile,
+                folder
+              );
+              req.body[fieldName] = fileUrl;
+            }
+          }
+        })
+      );
+    }
+
+    next();
+  } catch (error: any) {
+    next(createError(StatusCodes.BAD_REQUEST, error.message));
+  }
+};
+
+export default fileHandler;

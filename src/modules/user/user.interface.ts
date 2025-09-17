35e6b05ec11a92cf993ecd18e1642e1caf9bf6dc
Mir Tanim Ahmed <tanim@rentyard.com>
2025-09-17 07:24:03 +0600

    profile update, get and completion calculate

--- src/modules/user/user.interface.ts
+++ src/modules/user/user.interface.ts
@@ -3,40 +3,54 @@ import { Gender, UserRole } from "./user.constant";
 
 export interface IUser {
   _id: Types.ObjectId;
+  deviceTokens: string[];
 
   email: string;
   password: string;
   role: UserRole;
   verified: boolean;
-
   name: string;
-  companyName: string;
-  dateOfBirth: string;
-  gender: Gender;
-  nationality: string;
-  aboutMe: string;
-  salaryExpectations: string;
-  jobPreference: string;
+  dateOfBirth?: string | null;
+  gender?: Gender | null;
+  nationality?: string | null;
+  aboutMe?: string | null;
   languages: string[];
-
-  phoneNumber?: string;
-  linkedinProfile?: string;
-  personalWebsite?: string;
   address?: {
-    homeAddress?: string;
-    city?: string;
-    state?: string;
-    country?: string;
-    zipCode?: string;
+    homeAddress?: string | null;
+    city?: string | null;
+    state?: string | null;
+    country?: string | null;
+    zipCode?: string | null;
   };
+  phoneNumber?: string | null;
 
+  // For employer
+  linkedinProfile?: string | null;
+  personalWebsite?: string | null;
+
+  company?: {
+    name?: string;
+    industry?: string;
+    companySze?: string;
+    foundedYear?: string;
+    website?: string;
+    headQuarters?: string;
+    description?: string;
+    teamMembers?: string[];
+    totalCompetitions?: number;
+  };
+  hiringPreferences?: string[];
+
+  // For Employee
+  jobPreference?: string | null;
+  salaryExpectations?: string | null;
   experience: {
     company: string;
     jobTitle: string;
     startDate: string;
     endDate?: string;
     currentlyWorking: boolean;
-    jobDescription: string;
+    jobDescription?: string;
   }[];
 
   education: {
@@ -48,8 +62,8 @@ export interface IUser {
     endYear: string;
     description?: string;
   }[];
-
   skills: string[];
 
-  deviceTokens: string[];
+  profileCompletionPercentage: number;
+  isProfileComplete: boolean;
 }

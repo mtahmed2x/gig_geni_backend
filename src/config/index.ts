2c714438dce771d8fd46759927acff8a3783cf42
Mir Tanim Ahmed <mtahmed_dev@proton.me>
2025-10-24 17:04:46 +0600

    fixed wrong database connection url

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,27 +1,27 @@
-import dotenv from "dotenv";
-import path from "path";
-import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+import path from 'path';
+import nodemailer from 'nodemailer';
 
-dotenv.config({ path: path.resolve(process.cwd(), ".env") });
+dotenv.config({ path: path.resolve(process.cwd(), '.env') });
 
 export const config = {
   app: {
     port: Number(process.env.PORT) || 5000,
     socketPort: Number(process.env.SOCKET_PORT) || 6000,
-    env: process.env.NODE_ENV || "development",
+    env: process.env.NODE_ENV || 'development',
   },
   db: {
-    uri: process.env.MONGO_URI || "mongodb://127.0.0.1:27017/pharma-db",
+    uri: process.env.MONGO_URI!,
   },
   admin: {
     email: process.env.ADMIN_EMAIL!,
     password: process.env.ADMIN_PASSWORD!,
   },
   jwt: {
-    accessSecret: process.env.JWT_ACCESS_SECRET || "access",
-    accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || "15m",
-    refreshSecret: process.env.JWT_REFRESH_SECRET || "refresh",
-    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || "7d",
+    accessSecret: process.env.JWT_ACCESS_SECRET || 'access',
+    accessExpiresIn: process.env.JWT_ACCESS_EXPIRES_IN || '15m',
+    refreshSecret: process.env.JWT_REFRESH_SECRET || 'refresh',
+    refreshExpiresIn: process.env.JWT_REFRESH_EXPIRES_IN || '7d',
   },
 };
 
